3a6a6025376f45d9138fbc3a46b75aa64aa0c2f2
Md Yeasin <mdyeasinhn@gmail.com>
2024-10-24 22:42:00 +0600

    skill section updated

--- src/Components/Skils/SkilCard.jsx
+++ src/Components/Skils/SkilCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const SkilCard = () => {
+const SkilCard = ({ item }) => {
+    const { imgSrc, label, desc } = item;
+
     return (
-        <div>
-            card
+        <div className='flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 hover:ring-zinc-600 transition-colors group'>
+            {/* Add hover:ring to ensure something visible happens on hover */}
+            <div className='bg-zinc-700/50 rounded-lg  overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 transition-colors'>
+                <img src={imgSrc} alt={label} width={32} height={32} />
+            </div>
+            <div>
+                <h2>{label}</h2>
+                <p className='text-zinc-400 text-sm'>{desc}</p>
+            </div>
         </div>
     );
 };
--- src/Components/Skils/Skils.jsx
+++ src/Components/Skils/Skils.jsx
@@ -1,61 +1,76 @@
-import React from 'react';
+import SkilCard from './SkilCard';
+import figma from '../../assets/images/figma.svg';
+import css3 from '../../assets/images/css3.svg';
+import javascript from '../../assets/images/javascript.svg';
+import nodejs from '../../assets/images/nodejs.svg';
+import expressjs from '../../assets/images/expressjs.svg';
+import mongodb from '../../assets/images/mongodb.svg';
+import react from '../../assets/images/react.svg';
+import tailwindcss from '../../assets/images/tailwindcss.svg';
 
 const Skils = () => {
     const skillItem = [
         {
-            imgSrc: '/images/figma.svg',
+            imgSrc: figma,
             label: 'Figma',
             desc: 'Design tool'
         },
         {
-            imgSrc: '/images/css3.svg',
+            imgSrc: css3,
             label: 'CSS',
             desc: 'User Interface'
         },
         {
-            imgSrc: '/images/javascript.svg',
+            imgSrc: tailwindcss,
+            label: 'TailwindCSS',
+            desc: 'User Interface'
+        },
+        {
+            imgSrc: javascript,
             label: 'JavaScript',
             desc: 'Interaction'
         },
         {
-            imgSrc: '/images/nodejs.svg',
+            imgSrc: react,
+            label: 'React',
+            desc: 'Framework'
+        },
+
+        {
+            imgSrc: nodejs,
             label: 'NodeJS',
             desc: 'Web Server'
         },
         {
-            imgSrc: '/images/expressjs.svg',
+            imgSrc: expressjs,
             label: 'ExpressJS',
             desc: 'Node Framework'
         },
         {
-            imgSrc: '/images/mongodb.svg',
+            imgSrc: mongodb,
             label: 'MongoDB',
             desc: 'Database'
         },
-        {
-            imgSrc: '/images/react.svg',
-            label: 'React',
-            desc: 'Framework'
-        },
-        {
-            imgSrc: '/images/tailwindcss.svg',
-            label: 'TailwindCSS',
-            desc: 'User Interface'
-        },
+
+
     ];
+
     return (
-        <section className='section'>
+        <section className='mt-8'>
             <div className="container">
-                <h2 className="text-3xl lg:text-4xl  lg:leading-tight font-semibold">
+                <h2 className="text-3xl lg:text-4xl lg:leading-tight font-semibold">
                     Essential Tools I use
                 </h2>
-                <p className='text-zinc-400 mt-3 mb-8 max-w-[50ch]'>Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.</p>
+                <p className='text-zinc-400 mt-3 mb-8 max-w-[50ch]'>
+                    Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.
+                </p>
 
-                <div className=''>
-                    {skillItem.map(({imgSrc, label, desc}, key)=>{
-                        ("skil")
-                    })}
+                <div className='grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                    {skillItem.map((item, index) => (
+                        <SkilCard key={index} item={item} />
+                    ))}
                 </div>
+
             </div>
         </section>
     );
